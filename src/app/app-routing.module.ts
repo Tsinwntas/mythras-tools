1f7d2ac9aa5fdf8ddad61c1c8cceb569bc215672
Tsinik <nicholastsinwntas@gmail.com>
2023-03-14 13:52:43 -0400

    wrong base ref

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', redirectTo: './combat', pathMatch: 'full'},
-  { path: './combat', component: CombatFlowComponent }
+  {path: '', redirectTo: '/combat', pathMatch: 'full'},
+  { path: 'combat', component: CombatFlowComponent }
 ];
 
 @NgModule({
